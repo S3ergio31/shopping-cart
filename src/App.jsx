60f5ac1d0d6d4bcfcd16ca1c03fc051332bd31b2
Sergio Fidelis <sergiofidelis31@hotmail.com>
2022-06-14 00:09:31 -0300

    Updated App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
-import { Container } from '@mui/material';
-import Categories from './Categories';
-import NavBar from './NavBar';
-import PopularProducts from './PopularProducts';
-import SearchBar from './SearchBar';
+import { Container } from "@mui/material";
+import Categories from "./components/Categories";
+import NavBar from "./components/NavBar";
+import Products from "./components/Products";
+import SearchBar from "./components/SearchBar";
+import CartProvider from "./context/CartProvider";
+import useProducts from "./hooks/useProducts";
 
-const App = () =>  (
-  <>
-    <NavBar />
-    <Container style={{ paddingTop: '1rem' }}>
-      <SearchBar style={{ width: '100%'}}/>
-      <Categories />
-      <PopularProducts />
-    </Container>
-  </>
-);
+const App = () => {
+  const {
+    products,
+    filteredProducts,
+    selectedCategory,
+    loadingProducts,
+    loadingCategories,
+    categories,
+    handleSelectCategory,
+    handleSelectedProduct,
+  } = useProducts();
+
+  return (
+    <CartProvider>
+      <NavBar />
+      <Container sx={{ my: 1 }}>
+        <SearchBar
+          sx={{ width: 1 }}
+          products={products}
+          onSelect={handleSelectedProduct}
+        />
+        <Categories
+          onSelect={handleSelectCategory}
+          selected={selectedCategory}
+          categories={categories}
+          loading={loadingCategories}
+        />
+        <Products products={filteredProducts} loading={loadingProducts} />
+      </Container>
+    </CartProvider>
+  );
+};
 
 export default App;
-
