94768d9b884231eadcd65b707459458bd4360794
Sergio Fidelis <sergiofidelis31@hotmail.com>
2022-06-14 16:01:00 -0300

    Prop imgWidth fro ProductListItem

--- src/components/ProductListItem.jsx
+++ src/components/ProductListItem.jsx
@@ -4,9 +4,9 @@ const ProductListItem = (props) => (
   <Box component="li" sx={{ "& > img": { mr: 2, flexShrink: 0 } }} {...props}>
     <img
       loading="lazy"
-      width="20"
+      width={props.imgWidth || 20}
       src={props.product.image}
-      srcSet={`${props.product.image} 2x`}
+      srcSet={props.product.image}
       alt={props.product.title}
     />
     {props.product.title} - ${props.product.price}
