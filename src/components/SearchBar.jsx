8ce512c18eddeaa3817bbff74d3b7a1534d19ff2
Sergio Fidelis <sergiofidelis31@hotmail.com>
2022-06-14 00:08:49 -0300

    Created SearchBar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,29 @@
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import ProductListItem from "./ProductListItem";
+
+const SearchBar = ({ 
+  products, 
+  onSelect 
+}) => (
+  <Autocomplete
+    options={products}
+    autoHighlight
+    getOptionLabel={(product) => product.title}
+    onChange={(event, product) => onSelect(product)}
+    renderOption={(props, product) => (
+      <ProductListItem {...props} product={product} />
+    )}
+    renderInput={(params) => (
+      <TextField
+        {...params}
+        label="Search a product"
+        inputProps={{
+          ...params.inputProps
+        }}
+      />
+    )}
+  />
+);
+
+export default SearchBar;
