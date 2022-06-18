c0f4718bb8f4176f8a57881aa85bcaca60151f1c
Sergio Fidelis <sergiofidelis31@hotmail.com>
2022-06-17 22:13:45 -0300

    Redirect to home if cart is empty

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import CartProvider from "./context/CartProvider";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
+import CartHasProducts from "./Guards/CartHasProducts";
 
 const App = () => (
   <CartProvider>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/cart" element={<CartPage />} />
+      <Route path="/cart" element={<CartHasProducts Component={CartPage}/>} />
     </Routes>
   </CartProvider>
 );
---
+++ src/Guards/CartHasProducts.jsx
@@ -0,0 +1,13 @@
+import { CartContext } from "../context/CartProvider";
+import { Navigate } from "react-router-dom";
+import { useContext } from "react";
+
+const CartHasProducts = ({Component}) => {
+    const { cart } = useContext(CartContext);
+    if(cart.length){
+        return <Component />;
+    }
+    return <Navigate to="/" />;
+}
+
+export default CartHasProducts;
